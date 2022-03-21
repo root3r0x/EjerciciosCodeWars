d94db8942f338e86824ac726e57bcacd5a0feb30
Migue LD <miguel.gledesma.duarte@gmail.com>
2022-03-21 01:44:27 -0600

    Kata: Remove String Spaces.

---
+++ com/com/dia/cuatro/Kata.java
@@ -0,0 +1,15 @@
+package com.dia.cuatro;
+/**
+ * Remove String Spaces.
+ * Simple, remove the spaces from the string, then return the resultant string.
+ */
+
+/**
+ * @author ledes
+ *
+ */
+public class Kata {
+	public static String noSpace(final String x) {
+        return x;
+    }
+}
---
+++ com/com/dia/cuatro/SolutionTest.java
@@ -0,0 +1,9 @@
+package com.dia.cuatro;
+/**
+ * Remove String Spaces.
+ * Simple, remove the spaces from the string, then return the resultant string.
+ */
+
+public class SolutionTest {
+
+}
