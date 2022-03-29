e3bfe7f340442b418d2126b18801814a48641b7e
Migue LD <miguel.gledesma.duarte@gmail.com>
2022-03-28 23:07:36 -0600

    Kata día 6 OK, problema solucionado, pasa 1 de 1 tests.

--- com/com/dia/seis/Maps.java
+++ com/com/dia/seis/Maps.java
@@ -13,6 +13,10 @@ package com.dia.seis;
 
 public class Maps {
 	public static int[] map(int[] arr) {
+		
+		for(int i = 0; i< arr.length;i++) {
+			arr[i] = arr[i]*2;
+		}
 	    return arr;
     }
  }
