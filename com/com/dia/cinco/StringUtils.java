dfbe9c842a455cc2b0e18e3162c8313d415fb7a0
Migue LD <miguel.gledesma.duarte@gmail.com>
2022-03-28 22:35:41 -0600

    KataSpaces OK, problema solucionado, pasa 2 de 2 tests.

--- com/com/dia/cinco/StringUtils.java
+++ com/com/dia/cinco/StringUtils.java
@@ -16,48 +16,24 @@
 
 package com.dia.cinco;
 
-/**
- * @author ledes
- *
- */
-/**
-		for(int i =0; i>string.length(); i++)	{
-				
-			if(string.charAt(i)==Character.toUpperCase(string.charAt(i))) 
-			{
-				string.replace(string.charAt(i), Character.toLowerCase(string.charAt(i)));
-				i++;
-			}
-			if(string.charAt(i)==Character.toLowerCase(string.charAt(i))) {
-				string.replace(string.charAt(i), Character.toUpperCase(string.charAt(i)));
-				i++;
-			}
-			
-			//val1>val2 ? true : false;
-			char upper = string.charAt(i);
-			char lower = Character.toLowerCase(string.charAt(i));
-			
-			//upper=='o' ? string.replace(upper, lower) : string.replace(upper, lower);
-			
-			
-		} **/
 public class StringUtils {
 	
 	public static String toAlternativeString(String string) {
 		
-		// your code here!
-		String cadena = string;
+		String string2 = new String();		
+		for(int i =0;i<string.length();i++) {
 		
-		for(char c: cadena.toCharArray()) {
+			char oldchar = string.charAt(i);
+			char newchar = ' ';
 			
+			if (Character.isUpperCase(string.charAt(i))) {
+				newchar = Character.toLowerCase(oldchar);
+				string2 += newchar;		
+			}else {
+				newchar = Character.toUpperCase(oldchar);
+				string2 += newchar;	
+			}
 		}
-		
-		return cadena;
+		return string2;
 	  }
-	public static void main(String[] args) {
-		char a = 'b', b = 'b';
-		boolean  c = a==b;
-		
-		System.out.println( " Valor: " + c );
-	}
 }
