e1e2f5885ac8a1f770b6f2cc167fba13d8d3bb2b
Migue LD <miguel.gledesma.duarte@gmail.com>
2022-04-02 05:01:32 -0600

    Primera parte Ok, se toman las unidades.

--- com/com/dia/nueve/DescendingOrder.java
+++ com/com/dia/nueve/DescendingOrder.java
@@ -14,15 +14,64 @@
 
 package com.dia.nueve;
 
+import java.util.ArrayList;
+
 /**
  * @author ledes
  *
  */
+
 public class DescendingOrder {
 	
 	public static int sortDesc(final int num) {
 	    //Your code
-		return 0;
+		ArrayList<Integer> valores = new ArrayList<Integer>();
+		int maxVal = 0, mulDivVal = 10, pushVal =1;
+		
+		return num;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//variables
+		int numeroOriginal = 3452;
+		int multiplicador = 1, resultado = 0, contador = 0, acumulador=0;
+		int[] numeros;
+		
+		resultado = numeroOriginal / multiplicador;
+		System.out.println(".-Noriginal: [" + resultado + "].");
+		System.out.println("");
+		
+		while (resultado>0) {	
+			
+			//hasta que se realicen todas las diviciones posibles.
+			multiplicador *= 10;
+			
+			//Primer condicion
+			resultado = numeroOriginal / multiplicador;	
+			
+			System.out.println("[w] Resultado: [" + resultado + "].");
+			System.out.println("\t[m] =" + multiplicador);
+			contador++;
+		} 
+		System.out.println("");
+		
+		if(contador!=0) {
+			
+			numeros = new int[contador];
+			
+			for(int i = 0; i<numeros.length;i++){	
+				multiplicador = multiplicador/10;
+				numeros[i] = numeroOriginal/multiplicador;
+				System.out.println("No["+ i + "] : [" + numeros[i] + "].");
+				
+				numeroOriginal = numeroOriginal - (multiplicador* numeros[i]);
+			}
+		}
+		System.out.println("\n {" + contador + "}");
 	}
 }
 
